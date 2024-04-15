064bb695700c9ef96a4934ad90b9c446f8ac86af
thuphamhaley154 <166836070+thuphamhaley154@users.noreply.github.com>
2024-04-15 01:21:00 +0100

    Update Lesson4.sql

--- Lesson4.sql
+++ Lesson4.sql
@@ -44,15 +44,65 @@ ORDER BY film_id;
 
 
 --HAVING
---Hãy cho biết khách hàng nào đã trả tổng số tiền >100$
+--Hãy cho biết khách hàng nào đã trả tổng số tiền >100$, trong thg 1-2020
 SELECT * FROM payment
-SELECT customer_id,
+
+SELECT customer_id, --KO đc để payment_date vào đây, vì chỉ đc SELECT =GROUP BY
 SUM (amount) AS total_amount
 FROM payment
+WHERE payment_date BETWEEN '2020-01-01' AND '2020-02-01'
 GROUP BY customer_id
-HAVING SUM (amount) >100
---HAVING:lọc trên trường infor TỔNG HỢP: 
+HAVING SUM (amount) >10
+--HAVING:lọc trên trường infor TỔNG HỢP: SUM AVG, MIN, AMX --ALWAYS đứng sau GROUP BY
 --WHERE sd lọc trên trường có sẵn
+
+
+-- HAVING challenge: 2020, date 28,29,30/4 có doanh thu cao - giảm dần 
+--> AVG khoản payment(amount);NHÓM THEO-GROUP BY khách hàng và ngày payment-chỉ các KH nhiều hơn 1 khoản payment
+SELECT customer_id, DATE (payment_date),
+AVG (amount) AS avg_amount,
+COUNT (payment_id)
+FROM payment
+WHERE DATE (payment_date) IN ('2020-04-28', '2020-04-29', '2020-04-30') --hàm DATE để including GIỜ
+GROUP BY customer_id, DATE (payment_date)
+HAVING COUNT (payment_date) >1
+ORDER BY customer_id DESC
+
+--TOÁN TỬ VÀ HÀM SỐ HỌC
+SELECT 3+7, 3-7, 3*7, 7%3, 7^3
+SELECT film_id,
+rental_rate,
+ROUND (rental_rate*1.1,2) AS new_rental_rate,
+CEILING (rental_rate*1.1)-0.1 AS new_rental_rate
+FROM film
+/*Challenge: Quản lý của bạn đang nghĩ đến việc tăng giá cho những bộ 
+phim có chi phí thay thế cao. Vì vậy, bạn nên tạo một danh
+sách các bộ phim có giá thuê ít hơn 4% chi phí thay thế--->tỷ lệ giá thuê/CP thay thế<4%
+Tạo danh sách film_id đó cùng với tỷ lệ phần trăm (giá
+thuê/chi phí thay thế) được làm tròn đến 2 chữ số thập phân*/
+SELECT 
+film_id, 
+rental_rate, 
+replacement_cost,
+ROUND ((rental_rate/replacement_cost)*100,2) AS percentage
+FROM film
+WHERE ROUND ((rental_rate/replacement_cost)*100,2) <4 --KO đc lọc theo tên đã AS
+
+--TỔNG KẾT THỨ TỰ THỰC HIỆN CÂU LỆNH
+SELECT customer_id,
+COUNT (*) AS total_record
+FROM payment
+WHERE payment_date >='2020-01-30'  --1.tt sau ngày 30 -->WHERE
+GROUP BY customer_id
+HAVING COUNT (*) <=15
+ORDER BY total_record DESC
+LIMIT 5;
+--2.đếm xem có bn đơn đã tt -->COUNT
+--3.Mỗi KH đã có bn hóa đơn tt-->GROUP BY 1 trường ~SELECT 
+--4: chỉ hiện info các Kh có tổng số đơn tt <15 đơn -->HAVING, vì đang lọc theo ham Count rồi
+--5: xếp thứ tự các đơn hàng -->ORDER BY
+--6:chỉ hiện top 5--> LIMIT 5
+
 
 
 
