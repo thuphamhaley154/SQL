de40f803e5e26e7505c071b735adceffc4b8fb6c
thuphamhaley154 <166836070+thuphamhaley154@users.noreply.github.com>
2024-04-23 13:55:13 +0100

    Update Lesson10.sql

--- Lesson10.sql
+++ Lesson10.sql
@@ -18,3 +18,102 @@ INNER JOIN bookings.boarding_passes AS b
 ON a.seat_no=b.seat_no
 GROUP BY a.fare_conditions;
 
+
+--LEFT JOIN - RIGHT JOIN (table1 LEFT JOIN table2)
+SELECT t1.*, t2.* --thể hiện tt của t1,t2 sau khi lấy t1 LEFT JOIN t2 thông qua(ON) key1(t1),key2(t2)
+FROM table1 AS t1 --table1: gốc
+LEFT JOIN table2 AS t2 --table2: bảng tham chiếu 
+ON t1.key1=t2.key2 --ON thể hiện mqh giữa t1,t2 qua các keys
+--
+SELECT t1.*, t2.* 
+FROM table1 AS t1 --table1: bảng tham chiếu 
+RIGHT JOIN table2 AS t2 --table2: bảng GỐC
+ON t1.key1=t2.key2
+--Tìm thông in các chuyến bay của từng máy bay 
+--B1: xác định Table
+--B2: xác định key JOIN: aircraft_code
+--B3: chọn phương thức/loại JOIN 
+SELECT a.aircraft_code, b.flight_no
+FROM bookings.aircrafts_data AS a
+LEFT JOIN bookings.flights AS b
+ON a.aircraft_code=b.aircraft_code
+WHERE b.flight_no IS NULL --tìm máy bay đang ko có chuyến bay nào
+
+/*CHALLENGE: -tìm hiếu ghế nào đc chọn thường xuyên nhất (ensure all seats là đc liệt kê even chúng never được đặt)
+-có seat nào never đc đặt ko?
+-chỉ ra HÀNG ghế đc đặt thường xuyên nhất*/
+SELECT a.seat_no, --liệt kê các tt số ghế
+COUNT (flight_id) AS so_luong --đếm xem có bn chuyến bay 
+FROM bookings.seats AS a
+LEFT JOIN bookings.boarding_passes AS b
+ON a.seat_no=b.seat_no
+GROUP BY a.seat_no
+--ghế nào đc đăt thường xuyên nhất=> xsếp từ cao --> thấp
+ORDER BY COUNT (flight_id) DESC,
+--ghế ngồi never đc đặt: seat_no bảng a, ko có ở table b(bị Null)
+SELECT a.seat_no
+FROM bookings.seats AS a
+LEFT JOIN bookings.boarding_passes AS b
+ON a.seat_no=b.seat_no
+WHERE b.seat_no IS NULL;
+--chỉ ra HÀNG ghế đc đặt thường xuyên nhất
+SELECT RIGHT(a.seat_no,1) AS line, --liệt kê các tt số ghế
+COUNT (flight_id) AS so_luong --đếm xem có bn chuyến bay 
+FROM bookings.seats AS a
+LEFT JOIN bookings.boarding_passes AS b
+ON a.seat_no=b.seat_no
+GROUP BY RIGHT(a.seat_no,1)
+ORDER BY COUNT (flight_id) DESC;
+
+
+--FULL JOIN (table1 FULL JOIN table2)
+SELECT t1.*, t2.* 
+FROM table1 AS t1 
+FULL JOIN table2 AS t2 
+ON t1.key1=t2.key2
+--vd: 
+SELECT * FROM bookings.boarding_passes AS a
+FULL JOIN bookings.tickets AS b
+ON a.ticket_no=b.ticket_no
+--Tìm những vé máy bay mà ko đc cấp thẻ lên máy bay, đếm xem bn người=>COUNT(mean:có ticket_no,nhưng ko có boarding_no; có ở table a nhưng ko có ở table b)
+SELECT COUNT(*) 
+FROM bookings.boarding_passes AS a
+FULL JOIN bookings.tickets AS b
+ON a.ticket_no=b.ticket_no
+WHERE a.ticket_no IS NULL
+
+--JOIN ON MULTIPLE CONDITIONS
+--Tính GIÁ$ trung bình của từng số ghế máy bay 
+--B1: xác định output: số ghế, giá trung bình | input 
+SELECT * FROM bookings.seats 
+SELECT * FROM bookings.ticket_flights 
+ORDER BY ticket_no 
+/*PK:khóa chính trong table (chỉ có 1 thì mới JOIN đc), 
+table bookings.ticket_flights có 2 PK mean: có mã máy bay ở trong 2 bản ghi/dòng(1 vé máy bay trong 2 chuyến bay: vé quá cảnh-transit)*/   
+--> ko có tt nào để match giữa 2 table này 
+SELECT a.seat_no, 
+AVG(b.amount) AS avg_amount  --liệt kê số ghế, giá ghế
+FROM bookings.boarding_passes AS a
+LEFT JOIN bookings.ticket_flights AS b
+ON a.ticket_no=b.ticket_no 
+AND a.flight_id=b.flight_id
+--giá ghế TB
+GROUP BY a.seat_no
+ORDER BY AVG(b.amount) DESC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
