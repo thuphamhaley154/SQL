4cc32fedda074cfbc57515684bd8a1ebf67a3a19
thuphamhaley154 <166836070+thuphamhaley154@users.noreply.github.com>
2024-04-23 01:19:35 +0100

    Delete Lesson10.sql

--- Lesson10.sql
+++
@@ -1,7 +0,0 @@
---INNER JOIN (table 1 INNER JOIN table2)
-SELECT t1.*, t2.* --thể hiện tt của t1,t2 sau khi lấy t1 connect t2 thông qua(ON) key1(t1),key2(t2)
-FROM table1 AS t1
-INNER JOIN table2 AS t2
-ON t1.key1=t2.key2 --ON thể hiện mqh giữa t1,t2 qua các keys
-
-SELECT * FROM payment 
