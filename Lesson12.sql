40474c8f0799a05430e6706bfd0729c5ef288714
thuphamhaley154 <166836070+thuphamhaley154@users.noreply.github.com>
2024-04-29 00:26:36 +0100

    Update Lesson12.sql

--- Lesson12.sql
+++ Lesson12.sql
@@ -1,2 +1,5 @@
 --SUBQUERIES & CTEs
---
+
+--SUBQUERY IN WHERE: điều kiện lọc: cả hàm tổng hợp và hàm gốc ==> subquery. nếu SUBquery cần 1 kq nhất định, dùng dấu =; nếu 1 list thì dùng 'IN'
+--Tìm những hóa đơn có số tiền > số tiền TB các hóa đơn
+WHERE amount > (SELECT AVG(amount) FROM payment) 
