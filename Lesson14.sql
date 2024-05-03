37b7283340ed2bfcedac0b87fa55598ac1836093
thuphamhaley154 <166836070+thuphamhaley154@users.noreply.github.com>
2024-05-03 11:32:26 +0100

    Update Lesson14.sql

--- Lesson14.sql
+++ Lesson14.sql
@@ -47,7 +47,7 @@ COUNT (*) OVER (PARTITION BY customer_id, amount) as so_lan
 FROM payment
 ORDER BY payment_id
 
--- OVER (ODER BY...) 
+-- OVER (ORDER BY...) 
 --tính tổng doanh thu từ thời điểm trước đây đến hiện tại, lũy kế
 SELECT payment_date, amount, customer_id,
 --SUM(amount) OVER(ORDER BY payment_date) AS total_amount --ORDER BY: lũy kế dữ liệu amount
@@ -55,6 +55,66 @@ SUM(amount) OVER(PARTITION BY customer_id ORDER BY payment_date) AS total_amount
 FROM payment 
 
 
+--RANK
+--xếp hạng độ dài phim trong từng thể loại
+--output: film_id, length, category, xếp hạng length film trong từng category đó (xếp rank theo category)
+SELECT a.film_id, c.name as category, a.length,
+RANK() OVER(PARTITION BY c.name ORDER BY a.length DESC) AS rank1 ,
+DENSE_RANK() OVER (PARTITION BY c.name ORDER BY a.length DESC) AS rank2, --gối tiếp rank
+ROW_NUMBER () OVER (PARTITION BY c.name ORDER BY a.length DESC, a.film_id) AS rank3 -- sx thứ tự bị trùng nhau, orderby film_id 
+FROM film a
+JOIN film_category b ON a.film_id=b.film_id
+JOIN category c ON c.category_id=b.category_id
+ORDER BY c.name 
+-- nếu đề bài KO phân nhóm theo gì cả thì bỏ 'PARTITION BY..'
+
+--FIRST VALUE
+--số tiền tt cho đơn hàng đầu tiên và gần đây nhất của từng KH 
+SELECT * FROM 
+(SELECT customer_id, payment_date, amount, 
+ROW_NUMBER () OVER (PARTITION BY customer_id ORDER BY payment_date DESC ) AS stt 
+FROM payment ) AS a 
+WHERE stt=1;
+
+SELECT customer_id, payment_date, amount, 
+FIRST_VALUE(amount) OVER (PARTITION BY customer_id ORDER BY payment_date) AS frist_amount,
+--đơn hang gần đây nhất
+FIRST_VALUE(amount) OVER (PARTITION BY customer_id ORDER BY payment_date DESC) AS last_amount
+FROM payment;
+
+
+-- LEAD() , LAG ()
+
+--LEAD (): add cột có gtri liên tiếp sau đó 
+--Tìm chênh lệch sỐ tiền giữa các lần tt của từng KH 
+--Note: SQL chỉ đc +,-, * / theo chiều ngang, KO theo chiều dọc 
+--chênh lệch lần lien tiếp
+SELECT customer_id, payment_date, amount,
+LEAD(amount) OVER(PARTITION BY customer_id ORDER BY payment_date) AS next_amount,
+LEAD(payment_date) OVER(PARTITION BY customer_id ORDER BY payment_date) AS next_payment_date,
+amount- (LEAD(amount) OVER(PARTITION BY customer_id ORDER BY payment_date) )
+FROM payment
+--chênh lệch 3 lần liên tiếp
+SELECT customer_id, payment_date, amount,
+LEAD(amount,3) OVER(PARTITION BY customer_id ORDER BY payment_date) AS next_amount,
+LEAD(payment_date,3) OVER(PARTITION BY customer_id ORDER BY payment_date) AS next_payment_date,
+amount- (LEAD(amount) OVER(PARTITION BY customer_id ORDER BY payment_date) )
+FROM payment
+ 
+--LAG (): Thêm dòng dữ liệu có giá trị trước đó 
+SELECT customer_id, payment_date, amount,
+LAG(amount) OVER(PARTITION BY customer_id ORDER BY payment_date) AS next_amount,
+LAG(payment_date) OVER(PARTITION BY customer_id ORDER BY payment_date) AS next_payment_date,
+amount- (LAG(amount) OVER(PARTITION BY customer_id ORDER BY payment_date) )
+FROM payment
+
+--Nếu KO CẦN gom nhóm THEO CHIÊU CHÍ nào cả 
+--theo từng ngày sớ tiền chênh lệch giữa các lần tt là bn 
+SELECT customer_id, payment_date, amount,
+LAG(amount) OVER(ORDER BY payment_date) AS next_amount,
+LAG(payment_date) OVER(ORDER BY payment_date) AS next_payment_date,
+amount- (LAG(amount) OVER(ORDER BY payment_date) )
+FROM payment
 
 
 
