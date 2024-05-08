d4464ec3fafd61a5a306f109beb95fb591432a80
thuphamhaley154 <166836070+thuphamhaley154@users.noreply.github.com>
2024-05-08 14:23:54 +0100

    Update Lesson17-DDL&DML.sql

--- Lesson17-DDL&DML.sql
+++ Lesson17-DDL&DML.sql
@@ -72,6 +72,106 @@ SELECT * FROM Vcustomer_info
 
 DROP VIEW Vcustomer_info -- xóa câu lệnh 
 
+/*CHALLENGE: Tạo View có tên movies_category hiển thị ds các film 
+bao gồm title, length, category name được sắp xếp giảm dần theo length
+-Lọc kq để chỉ những films trong danh mục 'Action' và 'Comedy' */
+CREATE OR REPLACE VIEW movies_category AS
+(SELECT a.title, a.length, c.name as category_name 
+FROM film a
+JOIN public.film_category b ON a.film_id=b.film_id 
+JOIN public.category c ON b.category_id=c.category_id)
+
+SELECT * FROM movies_category 
+WHERE category_name IN ('Action', 'Comedy')
+
+--DDL: ALTER TABLE: thay đổi cấu trúc đối tượng của bảng 
+--ADD, DELETE columns
+ALTER TABLE manager
+DROP first_name -- xóa cột first_name 
+--thêm 1 trường tt nào đó 
+ALTER TABLE manager
+ADD column first_name VARCHAR(50)
+--RENAME columns: thay đổi tt cột nào đó trong bảng 
+ALTER TABLE manager
+RENAME column first_name TO ten_kh 
+--ALTER datatypes 
+ALTER TABLE manager 
+ALTER COLUMN ten_kh TYPE text  
+
+SELECT * FROM public.manager
+
+
+
+--DML: INSERT, UPDATE, DELETE, TRUNCATE 
+SELECT * FROM city 
+WHERE CITY_ID=1000 
+--INSERT: thêm 1 hoặc nhiều trường dữ liệu 
+INSERT INTO city --nếu ko nói rõ trường nào, thì auto insert vào all các trường tt
+VALUES (1000,'A',44,'2020-01-01 16:10:20'),
+(1001,'B',33,'2020-02-01 16:10:20' )
+--nếu insert chỉ 1 vài trường tt
+INSERT INTO city (city, country_id)
+VALUES ('C',55)
+--==>check 
+SELECT * FROM city 
+WHERE city= 'C'
+
+--UPDATE 
+--DML: INSERT, UPDATE, DELETE, TRUNCATE 
+--UPDATE : sửa dữ liệu 
+SELECT * FROM city WHERE city_id=3
+
+UPDATE city 
+SET country_id=100 --setup trường mk muốn thay đổi tt 
+WHERE city_id=3
+
+/*CHALLENGE: 1.Update giá thuê film 0.99 thành 1.99
+2. Điều chỉnh bảng customer như sau:
++Thêm cột initials (data type varchar(10))
++Update dữ liệu vào cột initials, vd: Frank Smith should be F.S */
+UPDATE film
+SET rental_rate=1.99
+WHERE rental_rate =0.99
+--CHECK
+SELECT * FROM film WHERE rental_rate =0.99 --NO
+--2,
+ALTER TABLE customer 
+ADD COLUMN initials VARCHAR(10);
+
+UPDATE customer 
+SET initials=LEFT(first_name,1) || '.'||LEFT(last_name,1)
+
+SELECT * FROM customer 
+
+
+--DELETE , TRUNCATE :xóa 1 hoặc all các dữ liệu trong 1 bảng nào đó 
+INSERT INTO manager
+VALUES(1,'HAPT','Tran','1997-01-01',20,'Ha'),
+(2,'THUMP','Adam','1990-01-01',33,'Haley'),
+(3,'GEOGE','Skem','2001-02-01',23,'Josh')
+
+SELECT * FROM manager 
+
+DELETE FROM manager 
+WHERE manager_id=1; --xóa data theo dữ liệu đk 
+--nếu muốn xóa the whole mà ko theo đk nào cả 
+DELETE FROM manager  --xóa dần dần 
+--or
+TRUNCATE TABLE manager  -- xóa toàn bộ 
+
+SELECT * FROM manager 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
