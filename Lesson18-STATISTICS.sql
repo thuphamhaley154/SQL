8a3e462f896b7564d0fc32091496f9eef7d36ba6
thuphamhaley154 <166836070+thuphamhaley154@users.noreply.github.com>
2024-05-12 07:52:04 +0100

    Create Lesson18-STATISTICS.sql

---
+++ Lesson18-STATISTICS.sql
@@ -0,0 +1,48 @@
+--BASIC STATISTICS & DETECT OUTLINER
+
+/* 1. Các khái niệm cơ bản trong thống kê
+2. Phân phối chuẩn
+3. Z-score
+4. Ứng dụngt tìm và xử lí dữ liệu ngoại lai (OUTLINER) */
+
+/* 1. Thống kê mô tả với Thống kê suy luận
+1. Thống kê mô tả : trình bày kq của mk dưới dạng table, biểu đồ, dashboard từ tập dữ liệ ban đầu để đưa ra 1 kq nào 
+có 2 thước đo chính : 
+- Trung bình, Trung vị, Yếu vị
+- Khoảng, Phương sai, Độ lệch chuẩn
+
+**Thống kê suy luận: chọn 1 mẫu trong 1 tổng thể, rồi tính toán số liệu mẫu đó rồi SUY ĐOÁN ra kq gần đúng cho tổng thể
+bao gồm các giả thuyết thử nghiệm và ước tính phát sinh. Phương pháp: tương quan, hồi quy
+
+2. Tổng thể và Mẫu, Thống kê và tham số
+- Tổng thể (Population): bao gồm all các đtg ngcuu
+- Mẫu (sample): là tập con lấy ra từ tổng thể
+
++ Thống kê: mô tả cho Mẫu
+  - Trung bình (X), Độ lệch chuẩn (S), cỡ mẫu (n) 
+  - Thống kê thường được dùng để ước lượng thổng thể
++ Tham số: mô tả cho Tổng thể
+  - Trung bình(μ), Độ lệch chuẩn (σ -SD-standard deviation), cỡ mẫu (N) 
+  - Tham số tổng thể thường KO BIẾT 
+
+
+3. Trung bình (Mean), Trung vị(Median), Yếu vị (Mode) 
+- Mean = tổng (gtri quan sát của mẫu OR tổng thể) / cỡ mẫu của mẫu OR tổng thể
+- Trung vị: Là điểm trong phân phối đánh dấu phân vị thứ 50. Tức 50% số quan sát nằm trên Trung vị và 50% dưới trung vị
+Cách tìm số trung vị: 
+   - Sắp xếp từ nhỏ đến lướn
+   - Nếu số lượng lẻ: số chính giữa là số median
+   - Nếu số lượng chắn: tính TB cộng 2 số giữa
+- Yếu vị: là quan sát xảy ra THƯỜNG xuyên nhất OR có tần suất cao nhất
+          1 mẫu có thể có 1 hoặc nhiều yếu vị
+
+
+
+4. Khoảng, Phương sai, Độ lệch chuẩn */
+
+
+
+
+
+
+
