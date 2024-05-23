687b587168a4289805b3ade56c556f88d8abe17a
thuphamhaley154 <166836070+thuphamhaley154@users.noreply.github.com>
2024-05-23 10:18:20 +0100

    Update and rename LESSON20. sql to LESSON20.sql

---
+++ LESSON20.sql
@@ -0,0 +1,7 @@
+--COHORT ANALYSIS (phân tích tổ hợp) USING SQL
+--ĐỂ Tìm ra nhu cầu về sp, insight KH: 'ptich hành vi của 1 nhóm KH' ;OR 'nhóm KH có chung 1 đạc điểm' trong 1 khoảng thời gian nhất định 
+--giúp xác định các xu hướng và hành vi của nhóm khách hàng, từ đó đưa ra các quyết định kinh doanh phù hợp.
+
+--CÁC loại COHORT CHART 
+
+
